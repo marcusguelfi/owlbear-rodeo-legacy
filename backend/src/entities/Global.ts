90fa37d3a2493639106f9bf0b6766bf3f489b805
Marcus Guelfi <marcusguelfi@gmail.com>
2025-11-30 20:21:17 -0300

    change port

--- backend/src/entities/Global.ts
+++ backend/src/entities/Global.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 export default class Global {
   static ORIGIN_WHITELIST: string = process.env.ALLOW_ORIGIN!!;
 
-  static CONNECTION_PORT: string | number = process.env.PORT || 9000;
+  static CONNECTION_PORT: string | number = process.env.PORT || 9001;
 
   static ICE_SERVERS = fs
     .readFile(path.resolve(__dirname, "../../", "ice.json"), "utf8")
